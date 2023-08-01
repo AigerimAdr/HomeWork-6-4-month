6623e56625fd391d31c6b3bf4e5acc8bef9f0bec
AigerimAdr <aigerim210596@gmail.com>
2023-08-01 23:50:32 +0600

    Выполнила ДЗ-6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './slide.css'
 
-function App() {
+const slideImg = [
+  {
+    imageUrl: "/img/pic-1.jpg",
+    alt: "pic-1",
+  },
+  {
+    imageUrl: "/img/pic-2.jpg",
+    alt: "pic-2",
+  },
+  {
+    imageUrl: "/img/pic-3.jpg",
+    alt: "pic-3",
+  },
+  {
+    imageUrl: "/img/pic-4.jpg",
+    alt: "pic-4",
+  },
+  {
+    imageUrl: "/img/pic-5.jpg",
+    alt: "pic-5",
+  }
+];
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const hasNext = index < slideImg.length - 1;
+  const hasPrev = index > 0;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } 
+  }
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    } else {
+      setIndex(0);
+    }
+  } 
+
+  let slide = slideImg[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        Nature
+      </h3>
+      <div className='slider'>
+        <img src={slide.imageUrl} alt={slide.alt}/>
+        </div>
+      <div className='slide-btn'>
+        <button onClick={handlePrevClick}>
+          Prev
+        </button>
+        <button onClick={handleNextClick}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
